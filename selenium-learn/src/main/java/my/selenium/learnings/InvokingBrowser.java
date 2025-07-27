4c72b203395ec2eb09e4328d7e4e886d234e99b1
SaravananJST <saravananmuthusamy2018@gmail.com>
2025-07-27 19:21:07 +0530

    update

---
+++ selenium-learn/src/main/java/my/selenium/learnings/InvokingBrowser.java
@@ -0,0 +1,14 @@
+package my.selenium.learnings;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class InvokingBrowser {
+
+	public static void main(String[] args) {
+		 ChromeDriver driver = new ChromeDriver();
+	        driver.get("https://google.com");
+	        driver.quit();
+
+	}
+
+}
